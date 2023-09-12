413d903ef59ea880f551bab2995d83988e3ad93e
Wajdy-E <143234994+Wajdy-E@users.noreply.github.com>
2023-09-12 12:44:02 -0400

    script.js

---
+++ script.js
@@ -0,0 +1,36 @@
+const observer = new IntersectionObserver((entries) =>{
+    entries.forEach((entry) =>{
+        console.log(entry);
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('.show')
+        }
+    });
+});
+
+const hiddenEl = document.querySelectorAll('.hidden-element');
+hiddenEl.forEach((el)=> observer.observe(el));
+
+
+document.getElementById("projects").addEventListener("click", function () {
+  document
+    .getElementById("shopify-projects")
+    .scrollIntoView({ behavior: "smooth" });
+});
+
+const accordionButtons = document.querySelectorAll(".accordion-button");
+const accordionCollapses = document.querySelectorAll(".accordion-collapse");
+
+accordionButtons.forEach((button) => {
+  button.addEventListener("click", () => {
+    accordionCollapses.forEach((collapse) => {
+      if (collapse !== button.nextElementSibling) {
+        collapse.classList.remove("show");
+      }
+    });
+  });
+});
+
+
+
