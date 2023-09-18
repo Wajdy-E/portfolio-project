9a73c5290558a7344355f79bb70bdcca079e2b06
Wajdy-E <143234994+Wajdy-E@users.noreply.github.com>
2023-09-17 21:44:16 -0400

    script.js

--- script.js
+++ script.js
@@ -1,23 +1,23 @@
-const observer = new IntersectionObserver((entries) =>{
-     entries.forEach((entry) =>{
-         console.log(entry);
-        if(entry.isIntersecting){
-            entry.target.classList.add('show');
-       }else{
-           entry.target.classList.remove('.show')
-         }
-     });
- });
+// const observer = new IntersectionObserver((entries) =>{
+//      entries.forEach((entry) =>{
+//          console.log(entry);
+//         if(entry.isIntersecting){
+//             entry.target.classList.add('show');
+//        }else{
+//            entry.target.classList.remove('.show')
+//          }
+//      });
+//  });
 
- const hiddenEl = document.querySelectorAll('.hidden-element');
-hiddenEl.forEach((el)=> observer.observe(el));
+//  const hiddenEl = document.querySelectorAll('.hidden-element');
+// hiddenEl.forEach((el)=> observer.observe(el));
 
 
-// document.getElementById("projects").addEventListener("click", function () {
-//   document
-//     .getElementById("shopify-projects")
-//     .scrollIntoView({ behavior: "smooth" });
-// });
+document.getElementById("projects").addEventListener("click", function () {
+  document
+    .getElementById("shopify-projects")
+    .scrollIntoView({ behavior: "smooth" });
+});
 
 const accordionButtons = document.querySelectorAll(".accordion-button");
 const accordionCollapses = document.querySelectorAll(".accordion-collapse");
